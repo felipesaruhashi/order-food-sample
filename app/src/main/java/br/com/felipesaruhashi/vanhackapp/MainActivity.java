f1d57b79befad419b240d2a80dfe013276754d16
Felipe Saruhashi <felipesaruhashi@MacBook-Pro-de-Felipe.local>
2018-03-18 10:04:07 -0300

    init app

---
+++ app/src/main/java/br/com/felipesaruhashi/vanhackapp/MainActivity.java
@@ -0,0 +1,13 @@
+package br.com.felipesaruhashi.vanhackapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
